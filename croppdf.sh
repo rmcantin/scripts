5fb3e426cf1f7d11d58855efb4f93066aeaa9b94
rmcantin <rmcantin@unizar.es>
2011-11-24 19:31:28 +0100

    Added some comments

--- croppdf.sh
+++ croppdf.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# File to download, crop and open a pdf ready for printing in multipage.
+# Intended to use for those book formated papers.
+#
+# Usage: ./croppdf.sh <URL of pdf>
+# Example: ./croppdf.sh http://users.isr.ist.utl.pt/~rmcantin/papers/tesis.pdf
+#
+# Author: Ruben Martinez-Cantin <rmcantin@unizar.es>
+#
 
 # Remove version number
 os=${OSTYPE//[0-9.]/}
